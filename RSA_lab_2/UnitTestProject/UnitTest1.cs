f4c315ca29ecc19bed2cbdf642b271e8a16b7d87
unknown <y.belyaeva@CORP10.abbyy.ua>
2015-03-03 15:42:32 +0200

    Add UnitTests

---
+++ RSA_lab_2/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RSA_lab_2.Controllers;
+using System.Numerics;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTotient()
+        {
+            Helper h = new Helper();
+            Assert.AreEqual(10, Helper.Totient(11));
+            Assert.AreEqual(1, Helper.Totient(1));
+            Assert.AreEqual(4, Helper.Totient(5));
+            Assert.AreEqual(4, Helper.Totient(8));
+            Assert.AreEqual(60, Helper.Totient(99));
+        }
+
+        [TestMethod]
+        public void GettingValueOfD()
+        {
+            Assert.AreEqual((BigInteger)6111579, BigInteger.Pow(3, Helper.Totient(9167368) - 1) % 9167368);
+            Assert.AreEqual(4, Math.Pow(3, Helper.Totient(11) - 1) % 11);
+        }
+
+        [TestMethod]
+        public void CheckC()
+        {
+            Assert.AreEqual(4051753, BigInteger.Pow(111111, 3) % 9173503);
+        }
+
+    }
+}
